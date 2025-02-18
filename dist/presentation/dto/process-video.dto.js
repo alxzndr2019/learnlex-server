92f049738209a53c9813377b09915f72c410c96f
Ohioze Alex Omiunu <ohiozeomiunu@gmail.com>
2025-02-18 14:26:36 +0000

    fix: linter fix

--- dist/presentation/dto/process-video.dto.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ProcessVideoDTO = void 0;
-const zod_1 = require("zod");
-exports.ProcessVideoDTO = zod_1.z.object({
-    url: zod_1.z.string().url(),
-    userId: zod_1.z.string(),
-});
